775784747af5cfae440a3e5f3b5f6a1fd522e886
Alan Crosswell <alan@columbia.edu>
2020-10-29 22:50:04 -0400

    duh wrong file path

--- mtk-rss.py
+++ mtk-rss.py
@@ -3,12 +3,13 @@ fg = FeedGenerator()
 fg.load_extension('podcast')
 
 fg.podcast.itunes_category('Music', 'Podcasting')
+file_url = 'https://fullduplexradio.us/audio/Full%20Duplex%20404%20-%202020-10-24.mp3'
 
 fe = fg.add_entry()
-fe.id('http://localhost/file.mp3')
+fe.id(file_url)
 fe.title('The First Episode')
 fe.description('Enjoy our first episode.')
-fe.enclosure('https://fullduplexradio.us/audio/Full%20Duplex%20404%20-%202020-10-24.mp3', 0, 'audio/mpeg')
+fe.enclosure(file_url, 0, 'audio/mpeg')
 fg.title('mtk')
 fg.description('some feed')
 fg.link(link={'href': 'https://fullduplexradio.us'})
